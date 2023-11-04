135d865006a5f42aea20044de10c94083909d24f
Fabio Miranda <web.jrhk@gmail.com>
2023-11-04 15:58:46 -0300

    People class created and starting to work with methods

---
+++ RegistrationSystem/Entities/People.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem.Entities
+{
+    class People
+    {
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public string MaritalStatus { get; set; }
+        public string PhoneNumber {  get; set; }
+        public bool OwnHouse { get; set; }
+        public bool Vehicle { get; set; }
+        public char Gender { get; set; }
+
+    }
+}
--- RegistrationSystem/Form1.cs
+++ RegistrationSystem/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RegistrationSystem.Entities;
 
 namespace RegistrationSystem
 {
     public partial class Form1 : Form
     {
+        List<People> peoples;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            peoples = new List<People>();
+
+            comboMS.Items.Add("Casado");
+            comboMS.Items.Add("Solteiro");
+            comboMS.Items.Add("Viúvo");
+            comboMS.Items.Add("Separado");
+
+            comboMS.SelectedIndex = 0;
         }
     }
 }
